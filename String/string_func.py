a8cb0356d082157e1225795cc3f2c54b1a6a6552
Jaehwan <woghks1213@gmail.com>
2020-04-17 19:56:56 +0900

    String commit 1

---
+++ String/string_func.py
@@ -0,0 +1,22 @@
+title = "TEAMLAB X Inflearn"
+
+# 모두 대문자 변환 upper()
+print(title.upper())
+
+# 모두 소문자 변환 lower()
+print(title.lower())
+
+# 각 단어의 앞글자만 대문자 변환 title()
+print(title.title())
+
+# 첫 번째 글자만 대문자 변환 capitalize()
+print(title.capitalize())
+
+# 변수에 특정 문자가 몇 개 있는지 개수 반환 count()
+print(title.count('a'))
+
+# 변수의 문자열이 숫자인지 여부 판단 isdigit()
+print(title.isdigit())
+
+# 변수가 특정 문자로 시작하는지 여부 판단 startswith()
+print(title.startswith('a'))
