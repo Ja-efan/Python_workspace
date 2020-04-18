9f0eb3d715e6ee6b5272282e326507756b0c93bd
Jaehwan <woghks1213@gmail.com>
2020-04-18 14:38:48 +0900

    String practice commit

---
+++ String/Practice/practice02.py
@@ -0,0 +1,5 @@
+fact = "Python is funny"
+
+print(fact.find('n'))
+print(fact.rfind('n'))
+print(str(fact.count('n') + fact.find('n') + fact.rfind('n')))
---
+++ String/Practice/practice07.py
@@ -0,0 +1,14 @@
+a = "abcd e f g"
+b = a.split()
+
+c = (a[:3][0])
+d = (b[:3][0][0])
+
+print(b)
+print(a[:3])
+print(c)
+print(b[:3])
+print(b[:3][0])
+print(d)
+
+print(c+d)
