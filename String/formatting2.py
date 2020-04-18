8c35483679e74681b49b451f2aa4987d56594d39
Jaehwan <woghks1213@gmail.com>
2020-04-18 13:34:54 +0900

    String commit 2

---
+++ String/formatting2.py
@@ -0,0 +1,12 @@
+# % 서식의 표현 기법
+# '%자료형 %(값)'
+# 넣고자 하는 값이 하나일 경우 괄호는 생략 가능, 두개 이상은 괄호 필요
+
+print("%s %s" % ("one","two"))
+
+print("%d %d" % (1,2))
+
+number = 3
+day = "three"
+
+print("I ate %d apples. I was sick for %s days." % (number, day))
---
+++ String/formatting3.py
@@ -0,0 +1,8 @@
+# format() 함수를 이용한 서식 지정
+# "{자료형}".format(인수)
+
+age = 23; name = 'Jaehwan'
+
+print("I'm {0} years old.".format(age))
+print("My name is {0} and {1} years old.".format(name,age))
+print("Product: {0}, Price per unit: {1:.2f}.".format("Apple",5.243))
